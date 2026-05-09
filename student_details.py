421a7d5d0923e80c7908b54fa10945004132df18
G.srusti <srustig0403@gmail.com>
2026-05-09 23:04:17 +0530

    Initialize student details in student_details.py
    Using data types and variables

---
+++ student_details.py
@@ -0,0 +1,13 @@
+student_name = 'srusti'
+age = 18
+standard = 12
+ph_no = "2348984561"
+
+student = {
+    'name': student_name,
+    'age': age,
+    'standard': standard,
+    'phone': ph_no
+}
+
+print(student)
