f4a48aaaacc3878b218b84c1e21339fb3338a385
G.srusti <srustig0403@gmail.com>
2026-05-10 22:46:47 +0530

    Add Instagram earnings calculator script
    Implement earnings calculation for Instagram influenced

---
+++ This is a simple Python project that calculates an Instagram influencer’s final earnings using variables, data types, and arithmetic operators. The program calculates total promotion earnings and subtracts internet expenses to display the final income.instagram_earnings_calculator.py
@@ -0,0 +1,8 @@
+name, promotions = 'srusti', 6
+earnings, internet = 2000, 500
+
+total = promotions * earnings
+final_income = total - internet
+
+print('Influencer Name =', name)
+print('Final Income =', final_income)
