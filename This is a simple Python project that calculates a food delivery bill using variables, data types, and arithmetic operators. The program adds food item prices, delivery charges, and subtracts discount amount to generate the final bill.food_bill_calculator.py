0367148e2a54690424489269a9fc8982a0578923
G.srusti <srustig0403@gmail.com>
2026-05-10 21:58:47 +0530

    food_bill_calculator.py
    This is a simple Python project that calculates a food delivery bill using variables, data types, and arithmetic operators. The program adds food item prices, delivery charges, and subtracts discount amount to generate the final bill.

---
+++ This is a simple Python project that calculates a food delivery bill using variables, data types, and arithmetic operators. The program adds food item prices, delivery charges, and subtracts discount amount to generate the final bill.food_bill_calculator.py
@@ -0,0 +1,21 @@
+print('---------food_delivery_charge------------')
+
+pizza_price = 80
+Burger_price = 50
+juice_price = 30
+deliver_charge = 30
+discount_amount = 20
+
+total_food_price = pizza_price + Burger_price + juice_price
+
+total_charge = total_food_price + deliver_charge
+
+bill = total_charge - discount_amount
+
+print('pizza_price =', pizza_price)
+print('Burger_price =', Burger_price)
+print('juice_price =', juice_price)
+
+print('total_charge =', total_charge)
+
+print('final_bill =', bill)
