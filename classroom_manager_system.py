8e1f5ee1061163046bb62e9fe2b30b4aa553bb23
G.srusti <srustig0403@gmail.com>
2026-05-12 21:10:47 +0530

    Implement classroom manager system with attendance

---
+++ classroom_manager_system.py
@@ -0,0 +1,32 @@
+print('========== CLASSROOM MANAGER SYSTEM ==========')
+
+# subjects
+subjects = ('maths', 'physics', 'kannada')
+
+# student names
+names = {'srusti', 'nandan', 'usha'}
+
+# student marks
+marks = {
+    'srusti': 59,
+    'nandan': 56,
+    'usha': 67
+}
+
+# attendance checking
+attendance = 80
+
+s = attendance > 75
+
+# output
+print('Subjects :', subjects)
+
+print('Student Names :', names)
+
+print('Student Marks :', marks)
+
+print('Attendance Percentage :', attendance)
+
+print('Attendance Eligible :', s)
+
+print('==============================================')
