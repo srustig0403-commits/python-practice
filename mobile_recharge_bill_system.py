d243be994f25106d046e0a671896e135d7784e43
G.srusti <srustig0403@gmail.com>
2026-05-18 22:20:42 +0530

    Create mobile_recharge_bill_system.py

---
+++ mobile_recharge_bill_system.py
@@ -0,0 +1,14 @@
+print('/===========Mobile Recharge Bill System=============')
+
+
+
+
+customer_detiles={'name':'srusti','ph_no':'7898678888'}
+
+recharge_amount=400
+amount=500
+primium=amount>500
+ print('customer is premium:', primium)
+ discount=200
+ final_amount=amount-discount
+ print('final_amount:',final_amount)
